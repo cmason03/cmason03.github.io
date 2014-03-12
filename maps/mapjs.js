8eef7eaa372a6526a993653cae803e3e14fecad6
cmason03 <corey.mason@tufts.edu>
2014-03-11 23:55:18 -0400

    lets do it

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -1,4 +1,8 @@
 
+
+
+function dropPersonPin()
+
 function getLocation()
   {
   if (navigator.geolocation)
@@ -7,33 +11,51 @@ function getLocation()
     }
   else{alert("Geolocation is not supported by this browser.");}
   }
-
-
   function initialize(position) {
         var mapOptions = {
           center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-          zoom: 13
+          zoom: 15
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
       }
 
-      google.maps.event.addDomListener(window, 'load', getLocation);
-/*
-   function dropPersonPin(position) {
-
-   	var image = 'MBTApic.png';
- var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-  var beachMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: image
-});
-   }
-
-google.maps.event.addDomListener(window, 'load', initialize);
-	*/
+window.onload = loadScript;  	
   
 
     
+      
+
+/*
+      function getMyLocation()
+			{
+				
+				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+					navigator.geolocation.getCurrentPosition(initialize); }
+						
+					
+
+				else {
+					alert("Geolocation is not supported by your web browser.  What a shame!");
+				}
+			} 
+
+      function initialize(position) {
+
+      var latlon=position.coords.latitude+","+position.coords.longitude;
+
+  var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
+  +latlon+"&zoom=14&size=400x300&sensor=false";
+  document.getElementById("mapholder").innerHTML="<img src='"+img_url+"'>";
+
+        var mapOptions = {
+          center: new google.maps.LatLng(myLat, myLng),
+          zoom: 8
+          
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+
+    */  
      
