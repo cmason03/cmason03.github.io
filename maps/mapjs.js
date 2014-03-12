a59bee1fc2827ed5fdab22433fbd9a8ae1a1abe3
cmason03 <corey.mason@tufts.edu>
2014-03-11 23:45:55 -0400

    lets do it

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -1,12 +1,13 @@
 
 
 
+function dropPersonPin()
 
 function getLocation()
   {
   if (navigator.geolocation)
     {
-    navigator.geolocation.getCurrentPosition(initialize);
+    navigator.geolocation.getCurrentPosition(initialize,dropPersonPin);
     }
   else{alert("Geolocation is not supported by this browser.");}
   }
@@ -18,42 +19,20 @@ function getLocation()
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
       }
+
+   function dropPersonPin(position) {
+
+   	var image = 'MBTApic.png';
+ var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+  var beachMarker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      icon: image
+});
+   }
+
 window.onload = loadScript;  	
   
 
     
-      
-
-/*
-      function getMyLocation()
-			{
-				
-				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-					navigator.geolocation.getCurrentPosition(initialize); }
-						
-					
-
-				else {
-					alert("Geolocation is not supported by your web browser.  What a shame!");
-				}
-			} 
-
-      function initialize(position) {
-
-      var latlon=position.coords.latitude+","+position.coords.longitude;
-
-  var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
-  +latlon+"&zoom=14&size=400x300&sensor=false";
-  document.getElementById("mapholder").innerHTML="<img src='"+img_url+"'>";
-
-        var mapOptions = {
-          center: new google.maps.LatLng(myLat, myLng),
-          zoom: 8
-          
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-
-    */  
      
