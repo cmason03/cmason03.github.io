aa46402bfebe2f9b246f75233f7de83754323c55
cmason03 <corey.mason@tufts.edu>
2014-03-11 23:34:08 -0400

    lets do it

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -18,8 +18,7 @@ function getLocation()
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
       }
-      google.maps.event.addDomListener(window, 'load', initialize);
-  	
+window.onload = loadScript;  	
   
 
     
