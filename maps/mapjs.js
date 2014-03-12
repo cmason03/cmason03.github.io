6a5a0d3204b2d947326efcc6c62ce2fe6c8c3dd1
cmason03 <corey.mason@tufts.edu>
2014-03-11 23:58:55 -0400

    lets do it

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -12,12 +12,20 @@ function getLocation()
   else{alert("Geolocation is not supported by this browser.");}
   }
   function initialize(position) {
+        
+        var mylatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		
         var mapOptions = {
           center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
           zoom: 15
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+
+        var myMarker = new google.maps.Marker ({
+			position: myLatLng,
+			map: map });
+
       }
 
 window.onload = loadScript;  	
