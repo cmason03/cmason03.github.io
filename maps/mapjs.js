ca4a9bcb0ab1acdd8f3f900b66087beaac12e348
cmason03 <corey.mason@tufts.edu>
2014-03-11 23:54:00 -0400

    lets do it

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -17,6 +17,7 @@ function getLocation()
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
       }
+      
       google.maps.event.addDomListener(window, 'load', initialize);
 /*
    function dropPersonPin(position) {
@@ -31,7 +32,7 @@ function getLocation()
    }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-	
+	*/
   
 
     
