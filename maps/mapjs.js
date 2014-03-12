51b079d880046b9b82178f565ddd2b3ce5ca57b4
cmason03 <corey.mason@tufts.edu>
2014-03-12 00:58:40 -0400

    i hate this

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -1,7 +1,7 @@
 
 
 function parse() {
-		var string = "[{"line":"Blue","station":"Airport","lat":42.374262,"lng":-71.030395},
+		var string = '[{"line":"Blue","station":"Airport","lat":42.374262,"lng":-71.030395},
 {"line":"Blue","station":"Aquarium","lat":42.359784,"lng":-71.051652},
 {"line":"Blue","station":"Beachmont","lat":42.39754234,"lng":-70.99231944},
 {"line":"Blue","station":"Bowdoin","lat":42.361365,"lng":-71.062037},
@@ -53,7 +53,7 @@ function parse() {
 {"line":"Red","station":"Savin Hill","lat":42.31129,"lng":-71.053331},
 {"line":"Red","station":"Shawmut","lat":42.29312583,"lng":-71.06573796},
 {"line":"Red","station":"South Station","lat":42.352271,"lng":-71.055242},
-{"line":"Red","station":"Wollaston","lat":42.2665139,"lng":-71.0203369}] "
+{"line":"Red","station":"Wollaston","lat":42.2665139,"lng":-71.0203369}]';
 
 parsed=JSON.parse(string);
 var numStations = Object.keys(parsed).length;
