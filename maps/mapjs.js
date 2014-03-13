64a5ce2b352ac8b12f4aa556f539099bdaec4afe
cmason03 <corey.mason@tufts.edu>
2014-03-13 13:39:17 -0400

    i hate this

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -91,7 +91,7 @@ var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
                 Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
                 Math.sin(dLon/2) * Math.sin(dLon/2);  
 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-var d = R * c; 
+var d = R * c * .621371; //.621371 converts km to miles
 
 
 return(d);
