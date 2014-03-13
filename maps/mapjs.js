29c48b596e050ee4429fa14cfcf4552f021ecb37
cmason03 <corey.mason@tufts.edu>
2014-03-13 12:49:05 -0400

    i hate this

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -79,9 +79,7 @@ var lon1 = myLocation.lng;
 var R = 6371; 
 var dLat = toRad(lat2-lat1);  
 var dLon = toRad(lon2-lon1);  
-var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-                Math.cos(toRad(lat1) * Math.cos(toRad(lat2)) * 
-                Math.sin(dLon/2) * Math.sin(dLon/2);  
+var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(toRad(lat1) * Math.cos(toRad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);  
 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 var d = R * c; 
 
