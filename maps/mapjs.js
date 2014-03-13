043bb120e1f91697abd24c2bf216df287a1f85de
cmason03 <corey.mason@tufts.edu>
2014-03-13 13:21:01 -0400

    i hate this

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -66,11 +66,14 @@ function trainPins(line, googleMap){
 	}
 }
 
-function toRad(x){
-	return x*(Math.Pi)/180;
-}
+
  
 function getDistance(myLocation,stationLocation){
+
+Number.prototype.toRad = function() {
+   return this * Math.PI / 180;
+}
+
 
 var lat2 = stationLocation.A; 
 console.log(lat2);
@@ -80,12 +83,16 @@ console.log(lat1);
 var lon1 = myLocation.K; 
 
 var R = 6371; 
-var dLat = toRad((/*lat2-*/lat1));  
-console.log(dLat);
-var dLon = toRad(lon2-lon1);  
-var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);  
+var x1 = lat2-lat1;
+var dLat = x1.toRad();  
+var x2 = lon2-lon1;
+var dLon = x2.toRad();  
+var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+                Math.sin(dLon/2) * Math.sin(dLon/2);  
 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 var d = R * c; 
+
 
 return(d);
 }
