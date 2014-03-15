3d9cd93bfa08307f05a77deb629336e589c3de28
cmason03 <corey.mason@tufts.edu>
2014-03-14 23:27:09 -0400

    i hate this

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -160,16 +160,13 @@ function addLineMarkers() {
 		if(parsed[i].line.toLowerCase() == line){
 			var goodStationObect=new Object();
 			goodStationObject = parsed[i];
-			console.log("i counter" + i);
 			goodStationArray.push(goodStationObject);
 			for(var j =0; j<scheduleData.schedule.length;j++){
-				console.log("j counter" + j);
 				for(var z=0; z<scheduleData.schedule[j].Predictions.length; z++){
-					console.log("z counter" + z);
 					if(scheduleData.schedule[j].Predictions[z].Stop == goodStationObject.station){
 						var predictions = [];
 						pred = scheduleData.schedule[j].Predictions;
-						console.log(pred);
+						console.log("predictions for" + goodstationObject.station + pred);
 						predictionsArray.push(predictions);
 						markerAndInfoWindow.push = {station:goodStationObject, schedule:pred, direction: scheduleData.schedule.Destination };
 					}
