ad13808bb8238b8027a9e93fdce5a91de2cc9a31
cmason03 <corey.mason@tufts.edu>
2014-03-15 02:04:35 -0400

    i hate this

--- maps/mapjs.js
+++ maps/mapjs.js
@@ -158,7 +158,6 @@ function closestStation(){
 function addMyMarker(){
 	//var closestToMe = closestStation();
 	//var contentString = "You are " + getDistance(myLocation, closestToMe[1]) + " from " + closestToMe[0];
-	console.log(contentString);
 
 
 }
